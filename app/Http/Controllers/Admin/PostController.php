4a08bdd91716e181405b474714098d873fd56a14
noemipintaldi <noemipintaldi@gmail.com>
2022-06-14 23:55:27 +0200

    migrations/seed tabelle ponte

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Category;
+use App\Models\Tag;
 use Illuminate\Support\Str;
 
 class PostController extends Controller
@@ -30,7 +31,8 @@ class PostController extends Controller
     public function create()
     {
         $categories = Category::all();
-        return view('admin.posts.create', compact('categories'));
+        $tags = Tag::all();
+        return view('admin.posts.create', compact('categories', 'tags'));
     }
 
     /**
